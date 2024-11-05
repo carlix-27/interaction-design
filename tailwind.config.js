408f69c0055851f68042977f407ccef0c08ea1fc
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-04 22:24:16 -0300

    add some sections primary, secondary, grey in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors:{
+      primary:{
+
+        400: '#B93DED',
+        500: '#A80CE8',
+        600: '#860ABA',
+
+
+      },
+      secondary:{
+
+      },
+      grey:{
+
+      }
+    },
+    fontSize:{
+
+    }
   },
   plugins: [],
 }
