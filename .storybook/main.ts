7b78ced422590fd21a8c57ada7c8f4f536a34059
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-06 10:07:58 -0300

    add cva

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,6 +15,9 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
+  // docs: {
+  //   autodocs: "tag",
+  // },
   staticDirs: ["..\\public"],
 };
 export default config;
