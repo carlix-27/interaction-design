1ce061f8dbc63c98f1adcefcbfff932f0c450d4e
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-06 09:58:50 -0300

    add sections primary, secondary, grey, fontSize in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         400: '#B93DED',
         500: '#A80CE8',
         600: '#860ABA',
+        700: '#65078B',
+        800: '#43055D',
+        900: '#22022E',
       },
       secondary:{
         500: '#00D4FF',
@@ -19,13 +22,22 @@ module.exports = {
       },
       grey:{
         50: '#F9FAFB',
+        100: '#F3F4F6',
         200: '#E5E7EA',
-
+        400: '#9EA2AE',
+        900: '#131927',
       }
     },
     fontSize:{
-
-
+      s1_subtitle: '18px',
+      s2_subtitle: '16px',
+      b1_body: '16px',
+      b1_bodyBold: '16px',
+      b2_body: '14px',
+      b2_bodyBold: '14px',
+      c1_caption: '12px',
+      c2_caption: '12px',
+      c3_caption: '10px',
     }
   },
   plugins: [],
