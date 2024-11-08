270e73363d3c0a8a04e320702bcb0c15b985aa4d
ValentinoGianuzzo <valentino.gianuzzo@ing.austral.edu.ar>
2024-11-08 18:02:15 -0300

    buttonPlus implemented corregido

--- src/components/course/ButtonPlus/ButtonPlus.tsx
+++ src/components/course/ButtonPlus/ButtonPlus.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from "../../../assets/icons/Icon";
 
 interface ButtonPlusProps {
     onClick?: () => void;
@@ -11,7 +12,7 @@ const ButtonPlus: React.FC<ButtonPlusProps> = ({ onClick }) => {
             className="flex items-center justify-center w-8 h-8 bg-[#a035f0] rounded-lg shadow-md hover:bg-[#8b2acb] focus:outline-none"
             aria-label="Add"
         >
-            <div className="h-6 w-6 text-white text-xl font-bold leading-none">+</div>
+            <Icon name="plus" />
         </button>
     );
 };
