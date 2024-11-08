58f0713b511d5469ff2514c7c7b2a659f9a356be
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-08 17:42:04 -0300

    Fix Card, PageControl

--- src/components/course/Card/Card.tsx
+++ src/components/course/Card/Card.tsx
@@ -5,14 +5,23 @@ interface CardProps {
     title: string;
     description: string;
     altText?: string;
+    type?: 'unit/project' | 'instructor';
 }
 
 
-const Card: React.FC<CardProps> = ({ image, title, description, altText = 'Image' }) => {
+const Card: React.FC<CardProps> = ({ image, title, description, altText = 'Image', type}) => {
     return (
-        <div className="flex flex-row items-center p-4 bg-white rounded-lg shadow-md max-w-md">
-            <img src={image} alt={altText} className="w-16 h-16 rounded-full mr-4 object-cover" />
-            <div className="flex flex-col gap-1">
+        <div className="flex flex-row items-center h-32 bg-grey-50 rounded-2xl shadow-md max-w-md">
+            <img
+                src={image}
+                alt={altText}
+                className={
+                         type === 'instructor'
+                         ? "w-24 h-24 rounded-full ml-4 mr-4 object-cover"
+                         : "w-32 h-full rounded-l-2xl object-cover"
+                }
+            />
+            <div className="flex flex-col ml-4 gap-1">
                 <h3 className="text-xl font-semibold text-gray-900">{title}</h3>
                 <p className="text-gray-500">{description}</p>
             </div>
