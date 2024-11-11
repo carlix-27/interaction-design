ca655a2de13e73946549acc56a6694ae57452be7
AnaB2 <ana.borda@ing.austral.edu.ar>
2024-11-11 16:41:25 -0300

    Add new changes

--- src/components/competeWithFriends/UserCard/UserCard.tsx
+++ src/components/competeWithFriends/UserCard/UserCard.tsx
@@ -2,8 +2,6 @@ import React, { HTMLAttributes } from 'react';
 import { cva, VariantProps } from 'class-variance-authority';
 import SwordIcon from '../SwordIcon/SwordIcon';
 
-
-
 const userCardVariant = cva('flex items-center p-4 bg-gray-50 rounded-lg shadow-md', {
     variants: {
         color: {
@@ -32,20 +30,17 @@ const UserCard: React.FC<UserCardProps> = ({ username, color, ...props }) => {
                  gap: '16px',
                  borderRadius: '16px',
                  boxShadow: '0px 10px 8px rgba(0, 0, 0, 0.3)',
-
-        }}
-
+             }}
              {...props}>
             {/* Icono de Perfil */}
-            <div className=" flex items-center justify-center mr-4" style={{ width:'60px', height:'82.29px'}} >
-        <span role="img" aria-label="profile" className="text-2xl text-pink-500">
-          <img src="/woman avatar (2).png" alt="profile" style={{ width: '100%', height: '100%' }} />
-        </span>
-
+            <div className="flex items-center justify-center mr-4" style={{ width: '60px', height: '82.286px', flexShrink: 0 }}>
+                <span role="img" aria-label="profile" className="text-2xl text-pink-500">
+                    <img src="/woman avatar (2).png" alt="profile" style={{ width: '100%', height: '100%' }} />
+                </span>
             </div>
 
             {/* Nombre de Usuario */}
-            <div className="flex flex-col  justify-center items-start flex-1 text-lg font-semibold text-gray-900" style={{
+            <div className="flex flex-col justify-center items-start flex-1 text-lg font-semibold text-gray-900" style={{
                 color: '#000',
                 textAlign: 'center',
                 fontFamily: 'Roboto',
