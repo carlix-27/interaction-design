8b9769bf5cd2a5a9a0761586a95787eb7d8a01f1
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-12 19:01:00 -0300

    Fix card component. Instructor

--- src/components/course/Card/Card.tsx
+++ src/components/course/Card/Card.tsx
@@ -16,7 +16,7 @@ const Card: React.FC<CardProps> = ({
   type,
 }) => {
   return (
-    <div className="flex flex-row items-center p-1 bg-grey-50 rounded-2xl shadow-md max-w-md">
+    <div className="flex flex-row items-center p-2 bg-grey-50 rounded-2xl shadow-md max-w-sm max-h-fit">
       <img
         src={image}
         alt={altText}
@@ -27,7 +27,7 @@ const Card: React.FC<CardProps> = ({
         }
       />
       <div className="flex flex-col ml-4 gap-1 text-left">
-        <p className="text-xl font-subtitleFont text-subtitle2 text-gray-900">
+        <p className="text-xl fontFamily-subtitleFont fontSize-subtitle2 font-semibold text-gray-900">
           {title}
         </p>
         <p className="text-gray-500 font-captionFont text-body2">
