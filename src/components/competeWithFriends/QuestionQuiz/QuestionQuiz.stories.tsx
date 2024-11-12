d34346c3e93facffebad0ec855a4a02b0378173f
AnaB2 <ana.borda@ing.austral.edu.ar>
2024-11-12 10:35:47 -0300

    Add QuestionQuiz component

---
+++ src/components/competeWithFriends/QuestionQuiz/QuestionQuiz.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import QuestionQuiz from './QuestionQuiz';
+
+const meta: Meta<typeof QuestionQuiz> = {
+    title: 'QuestionQuiz',
+    component: QuestionQuiz,
+    argTypes: {
+        question: {
+            control: 'text',
+            defaultValue: '¿Cuál es la capital de España?',
+        },
+        currentQuestion: {
+            control: 'number',
+            defaultValue: 3,
+        },
+        totalQuestions: {
+            control: 'number',
+            defaultValue: 10,
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof QuestionQuiz>;
+
+export const Default: Story = {
+    args: {
+        question: '¿Cuál es la capital de España?',
+        currentQuestion: 3,
+        totalQuestions: 10,
+    },
+};
---
+++ src/components/competeWithFriends/QuestionQuiz/QuestionQuiz.tsx
@@ -0,0 +1,41 @@
+import React, { HTMLAttributes } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+
+const questionQuizVariant = cva([
+    "rounded-lg",
+    "bg-white",
+    "shadow-lg",
+    "flex",
+    "w-80",
+    "h-44",
+    "flex-col",
+    "items-center",
+    "justify-center", // Centra los elementos verticalmente
+]);
+
+interface QuestionQuizProps extends Omit<HTMLAttributes<HTMLDivElement>, "any"> {
+    question: string;
+    currentQuestion: number;
+    totalQuestions: number;
+}
+
+const QuestionQuiz: React.FC<QuestionQuizProps> = ({ question, currentQuestion, totalQuestions }) => {
+    return (
+        <div className="relative p-4 rounded-2xl overflow-hidden bg-white shadow-lg flex w-80 h-44 flex-col items-center justify-center">
+            {/* Contador de preguntas */}
+            <div className="absolute top-2 left-1/2 transform -translate-x-1/2 text-center font-inter text-xs font-medium leading-4 uppercase text-grey-500">
+                {currentQuestion}/{totalQuestions}
+            </div>
+
+            {/* Texto de la pregunta */}
+            <h1 className="text-center font-roboto text-lg font-semibold leading-6 w-full text-black mt-6">
+                {question}
+            </h1>
+
+            {/* Barra de progreso */}
+            <div className="absolute bottom-0 left-0 w-36 h-1 bg-primary-700 "></div>
+        </div>
+    );
+};
+
+export default QuestionQuiz;
