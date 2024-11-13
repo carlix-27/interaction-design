38eb79a2e3a1e3980227d3427c3c05640af4c5f1
franco <franco.aparicio@ing.austral.edu.ar>
2024-11-13 15:29:05 -0300

    Added spinner to visualizeContent

--- src/components/common/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner/LoadingSpinner.tsx
@@ -11,18 +11,20 @@ const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
   color = "purple-500",
 }) => {
   return (
-    <div className="flex items-center space-x-2">
-      {[...Array(16)].map((_, index) => (
-        <div
-          key={index}
-          className={`animate-spin rounded-full border-t-2 border-${color}`}
-          style={{
-            width: size - index * 4,
-            height: size - index * 4,
-            borderTopColor: `rgba(128, 0, 128, ${1 - index * 0.2})`,
-          }}
-        ></div>
-      ))}
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center space-x-2">
+        {[...Array(1)].map((_, index) => (
+          <div
+            key={index}
+            className={`animate-spin rounded-full border-t-2 border-${color}`}
+            style={{
+              width: size - index * 4,
+              height: size - index * 4,
+              borderTopColor: `rgba(128, 0, 128, ${1 - index * 0.2})`,
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
