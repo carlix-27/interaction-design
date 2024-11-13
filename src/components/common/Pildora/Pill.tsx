fb2082f46a9fbca5826646f22eed4a03fd9c62f6
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-13 17:23:30 -0300

    fix pill

--- src/components/common/Pildora/Pill.tsx
+++ src/components/common/Pildora/Pill.tsx
@@ -2,19 +2,19 @@ import React, { HTMLAttributes } from 'react';
 import { cva, VariantProps } from "class-variance-authority";
 
 
-const pillVariant = cva(['flex font-buttonFont font-buttonFont justify-center items-center flex-shrink-0 rounded-[32px] shadow-md'], {
+const pillVariant = cva(['flex fontSize-buttonText fontFamily-buttonFont justify-center items-center flex-shrink-0 rounded-[32px] shadow-md'], {
     variants:{
         variant:{
             default:[
-                "w-32 h-9 bg-grey-50 text-primary-500",
+                "w-[120px] h-[35px] bg-grey-50 text-primary-500",
                 "box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);"
             ],
             selected: [
-                "w-32 h-9 bg-primary-500 text-white",
+                "w-[120px] h-[35px] bg-primary-500 text-white",
                 "box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);"
             ],
             disabled:[
-                "w-32 h-9 bg-grey-100 text-grey-400",
+                "w-[120px] h-[35px] bg-grey-100 text-grey-400",
                 "box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);"
             ]
         },
