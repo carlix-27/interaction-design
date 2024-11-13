25c48be547c2193dec86b3de32ab895a36f1e553
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-11-13 13:09:13 -0300

    Fix some components but i finish this later

--- src/assets/icons/Icon.tsx
+++ src/assets/icons/Icon.tsx
@@ -607,9 +607,10 @@ const Icon: React.FC<IconProps> = ({ name }) => {
 
     case "battle":
       return (
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
-            <circle cx="20" cy="20" r="20" fill="#65078B"/>
-          </svg>
+          <img
+              src="/Battle%20(1).png"
+              alt="Battle Icon"
+          />
       );
 
     default:
--- src/components/competeWithFriends/SwordIcon/SwordIcon.tsx
+++ src/components/competeWithFriends/SwordIcon/SwordIcon.tsx
@@ -1,32 +1,17 @@
-import React, { HTMLAttributes } from 'react';
-import { cva, VariantProps } from 'class-variance-authority';
+import React from 'react';
 import Icon from "../../../assets/icons/Icon";
 
-const swordIconVariant = cva('flex items-center justify-center rounded-full', {
-    variants: {
-        color: {
-            purple: 'bg-primary-700 text-white',
-        },
-    },
-    defaultVariants: {
-        color: 'purple',
-    },
-});
 
-interface SwordIconProps
-    extends Omit<HTMLAttributes<HTMLDivElement>, 'color'>,
-        VariantProps<typeof swordIconVariant> {}
 
-const SwordIcon: React.FC<SwordIconProps> = ({ color, ...props }) => {
+
+const SwordIcon: React.FC = () => {
     return (
-        <div
-            className={`${swordIconVariant({ color })} w-[40px] h-[40px] text-2xl shadow-[0px_10px_8px_rgba(0,_0,_0,_0.3)]`}
-            {...props}
-        >
-            <span className="flex items-center justify-center w-full h-full">
+        <div className="flex items-center justify-center w-full h-full">
+            <div className="flex items-center justify-center p-2 w-8 h-8 rounded-full bg-primary-700">
                 <Icon name='battle'></Icon>
-            </span>
+            </div>
         </div>
+
     );
 };
 
